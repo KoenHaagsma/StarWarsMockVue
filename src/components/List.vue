0b7ac3a41763427aa8d4c466ad5412de413adf31
Koen Haagsma <koenhaagsma123@hotmail.nl>
2022-11-24 09:53:46 +0100

    Setup

---
+++ src/components/List.vue
@@ -0,0 +1,49 @@
+<template>
+    <ul v-for="person in peoples" :key="person.name">
+        <li>
+            <h2>{{ person.name }}</h2>
+        </li>
+    </ul>
+</template>
+
+<script lang="ts">
+const url = 'https://swapi.dev/api/people/';
+export default {
+    data(): Persons {
+        return {
+            peoples: [],
+        };
+    },
+    async mounted() {
+        try {
+            const response = await fetch(url);
+            this.peoples = await response.json();
+        } catch (e: any) {
+            console.warn(e.message);
+        }
+    },
+};
+
+interface Persons {
+    peoples: Person[];
+}
+
+type Person = {
+    name: string;
+    height: string;
+    mass: string;
+    hair_color: string;
+    skin_color: string;
+    eye_color: string;
+    birth_year: string;
+    gender: string;
+    homeworld: string;
+    films: string[];
+    species: string[];
+    vehicles: string[];
+    starships: string[];
+    created: string;
+    edited: string;
+    url: string;
+};
+</script>
